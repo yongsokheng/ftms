45ee03a5eed24358e5c04e005757bfe56d409d6e
qtsoul95 <qtsoul95@gmail.com>
2016-08-20 08:29:46 +0700

    Update when receive message

--- app/assets/javascripts/channels/chat_rooms.js
+++ app/assets/javascripts/channels/chat_rooms.js
@@ -8,10 +8,23 @@ App.global_chat = App.cable.subscriptions.create({
   },
 
   received: function(data) {
+    var room = $(".chat-room[data-id='" + data["channel_id"] + "'][data-type='" + data["channel_type"] + "']");
     message_list = $(".messages-list");
-    message = message_list.find("#message-" + data["id"]);
+    message = message_list.find("#message-" + data["message_id"]);
+
     if (message.length == 0) {
-      message_list.append(data["message"]);
+      if (room.hasClass("active-room")) {
+        message_list.append(data["message"]);
+      } else {
+        $(".sidebar-menu").prepend(room);
+        var new_message = room.find(".badge");
+        var new_message_count = parseInt(new_message.text());
+        if (isNaN(new_message_count)) {
+          new_message_count = 0;
+        }
+        new_message_count ++;
+        new_message.text(new_message_count);
+      }
     } else {
       message.replaceWith(data["message"]);
     }
