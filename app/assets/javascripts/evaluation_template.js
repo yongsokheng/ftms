056fd6ce6c4f30b2b5d768cb4cc089315a0b50b4
qtsoul95 <qtsoul95@gmail.com>
2016-08-31 14:27:57 +0700

    Fix validate evaluation & cancan redirect & clone course

--- app/assets/javascripts/evaluation_template.js
+++ app/assets/javascripts/evaluation_template.js
@@ -3,4 +3,8 @@ $(document).on("turbolinks:load", function() {
   if(tbl_evaluation_template.length > 0) {
     set_datatable(tbl_evaluation_template, [0, 2, 3, 4, 5]);
   }
+
+  $('#evaluation_template_min_point').unbind('focus').on('focus', function () {
+    $(this).attr('max', $('#evaluation_template_max_point').val());
+  });
 });
