0e6de405957f6b3383125e4628829526efac2105
ngengchhengkim <chhengkimngeng@gmail.com>
2016-09-05 11:21:07 +0700

    load lai bieu do khi update task

--- app/assets/javascripts/subject.js
+++ app/assets/javascripts/subject.js
@@ -1,4 +1,4 @@
-$(document).on('turbolinks:load', function() {
+$(document).on('turbolinks:load ajaxComplete', function() {
   var tbl_subject = $('#subjects');
   if(tbl_subject.length > 0) {
     set_datatable(tbl_subject, [0, 2]);
